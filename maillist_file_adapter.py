f08e2a8eb5a343ccd59dbdc1be90465a331abfd6
Tether Ball Shark <tetherballshark@mint>
2014-03-27 20:08:57 +0200

    Bug fixed

--- maillist_file_adapter.py
+++ maillist_file_adapter.py
@@ -49,7 +49,8 @@ class MailListFileAdapter():
 
         for unparsed_subscriber in lines:
             subscriber = unparsed_subscriber.split(" - ")
-            result.add_subscriber(subscriber[0], subscriber[1])
+            if subscriber[0] and subscriber[1]:
+                result.add_subscriber(subscriber[0], subscriber[1])
 
         return result
 
